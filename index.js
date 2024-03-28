cdb3ff04b36b6b9cee73d1ac75659d36fbf63b15
Gresey Patidar <greseypatidar100@gmail.com>
2024-03-28 19:52:15 +0530

    css file removed temporarily

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import { nanoid } from "nanoid";
 import URL from "./models/urlModel.js";
 import cors from 'cors';
 import urlExist from "url-exist";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/user.js";
+import { restrictToLoggedinUserOnly } from "./middleware/auth.js";
 
 const app = express();
 
@@ -27,7 +30,7 @@ app.set("view engine","ejs");
 app.set('views',path.resolve('./views'));
 
 
-
+app.use(cookieParser());
 dotenv.config();
 
 //middleware to validate url
@@ -42,17 +45,34 @@ const validateUrl=async(req,res,next)=>{
 
 // Serve the index.html file
 app.get("/", async(req, res) => {
-    res.render("index.ejs",{id:null});
+    const allUrls=await URL.find({});
+    return res.render("index",{url:allUrls,id:null});
+    
   
 });
 
+app.use("/user",userRoute);
+
+app.get("/signup",(req,res)=>{
+    return res.render("signup");
+});
+app.get("/login",(req,res)=>{
+    return res.render("login");
+});
+
+
 // Handle POST requests to shorten URLs
-app.post("/url",validateUrl, async (req, res) => {
+app.post("/url",restrictToLoggedinUserOnly, async (req, res) => {
     const url = req.body.url;
     const id = nanoid(8);
-    const newurl = new URL({ url, id });
+   // const newurl = new URL({ url, id });
+   const newurl=await URL.create({
+    url:url,
+    id:id,
+    createdby:req.user._id,
+   })
     try {
-        await newurl.save();
+       // await newurl.save();
         const domain = process.env.PRODUCTION_URL || 'http://localhost:8000/';
        const shorturl= `${domain}${newurl.id}`
         res.render("index.ejs",{id:shorturl});
