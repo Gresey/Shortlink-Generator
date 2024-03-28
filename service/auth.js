1852879b6a7a1bab6fb9f76acdf84955e60abf4f
Gresey Patidar <greseypatidar100@gmail.com>
2024-03-28 19:48:49 +0530

    Mapping of user object to id done

---
+++ service/auth.js
@@ -0,0 +1,7 @@
+const sessionIdToUserMap=new Map();
+export function setUser(id,user){
+    sessionIdToUserMap.set(id,user);
+}
+export function getUser(id){
+    return sessionIdToUserMap.get(id);
+}
