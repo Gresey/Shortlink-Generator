6e862552a40905d31b192b6e381019c42ed0f5fe
Gresey Patidar <greseypatidar100@gmail.com>
2024-03-28 19:46:47 +0530

    urlmodel added

--- models/urlModel.js
+++ models/urlModel.js
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
 
 const urlSchema=new mongoose.Schema({
- url:{
+url:{
     type:String,
     required:true,
     unique:true,
  },
- id:{
+id:{
     unique:true,
     type:String,
+ },
+ createdby:{
+   type:mongoose.Schema.Types.ObjectId,
+   ref:"users",
  }
 
 });
 
 const urlmodel=mongoose.model("URL",urlSchema);
 export default urlmodel;
-
